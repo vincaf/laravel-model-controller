e3e6d7e32973766b5031e56fc7c18139eff6a138
vincaf <vincenzocafassog@gmail.com>
2022-09-09 00:50:03 +0200

    feat: added name to Routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
-Route::get('/movies', 'MovieController@index');
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/movies', 'MovieController@index')->name('movies');
