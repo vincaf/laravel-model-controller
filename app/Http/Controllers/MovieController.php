bb9ac39d856cd18e7fcdf5e32648bf6789aeb819
vincaf <vincenzocafassog@gmail.com>
2022-09-09 00:20:25 +0200

    feat: add controllers and blades for the structure

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use App\Movie;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class MovieController extends Controller
 {
     //
     public function index(){
-        return view('home.index');
+        $movies = Movie::all();
+        return view('movies.index', compact('movies'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
+Route::get('/movies', 'MovieController@index');
